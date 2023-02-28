c632a5cbe1aba0df571f07f56570e54eb385814d
weiliangna <126581577+weiliangna@users.noreply.github.com>
2023-02-28 22:35:41 +0800

    Update ukids.js

--- ukids.js
+++ ukids.js
@@ -1,8 +1,9 @@
 
 /***
+作者：微凉
+软件下载：商店搜索“小小优趣”
 使用声明：️此脚本仅供学习与交流，
         请勿转载与贩卖！️️️
-        软件下载：商店搜索“小小优趣”
 *******************************
 [rewrite_local]
 ^http[s]?:\/\/((prod)|(fastapi))\.ukids\.cn\/(uch5|ucapp)\/(getUser|sync).*$ url script-response-body ukids.js
